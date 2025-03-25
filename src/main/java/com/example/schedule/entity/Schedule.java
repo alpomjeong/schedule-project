f490432c0edd174869602fbc8e0d37327a18b809
alpomjeong <jsw00415@gmail.com>
2025-03-25 15:46:50 +0900

    feat : 기본 클래스 Schedule.java 작성
    Schedule.java 멤버 변수 및 생성자 작성

--- src/main/java/com/example/schedule/entity/Schedule.java
+++ src/main/java/com/example/schedule/entity/Schedule.java
@@ -1,4 +1,23 @@
 package com.example.schedule.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class Schedule {
+    private long id;
+    private String todo;
+    private String author;
+    private String password;
+    private String created_at;
+    private String updated_at;
+
+    public Schedule(String todo, String author, String password, String created_at, String updated_at) {
+        this.todo = todo;
+        this.author = author;
+        this.password = password;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
 }
